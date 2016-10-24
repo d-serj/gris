04557b0874050daccc2c70be18862ebccb501a04
d-serj <sp11kenny@gmail.com>
2016-10-24 15:46:16 +0300

    Update changing_automat.cpp

--- changing_automat.cpp
+++ changing_automat.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
         }
         else
         {
-            printf (rus ("Ваша сдача: \n"));   
+            printf (rus ("Ваша  сдача: \n"));   
                 
 			change = cash - cost;
 
