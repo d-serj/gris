7b9d4fc8ba5d76ed7a9706ae96d05c18aa7a9903
d-serj <sp11kenny@gmail.com>
2016-11-24 22:32:01 +0200

    Update full_calendar_new.c

--- full_calendar_new.c
+++ full_calendar_new.c
@@ -29,20 +29,35 @@ void setcolor (int i)
 }
 
 void print_calendar (int month, int year, int horizontal, int vertical);
-int celebrate (int year, int month, int day);
-int pasha_p (int year);
+//int celebrate (int year, int month, int day);
+void pasha_p (int year, int couter);
 
 char *name[] = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
 int  *days_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
+                               //1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+int *celebrateDays[12][31] = {  {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Январь
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, // Февраль
+                                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Март
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Апрель
+                                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Май
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, // Июнь
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Июль
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, // Август
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Сентябрь
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Октябрь
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Ноябрь
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}  // Декабрь
+                            };
 int main()
 {
-    int  i, y, month, year, horizontal, vertical;
+    int  i, y, month, year, horizontal, vertical, counter;
 
     system ("mode 70, 40");
 
     for (;;)
     {
+        counter = 0;  // Счетчик начала цикла
+
         printf (rus ("Введите год: "));
         scanf ("%d", &year);
 
@@ -54,6 +69,8 @@ int main()
         horizontal = 1;
         vertical   = 2;
         month      = 1;
+
+        pasha_p(year, counter);
 
         // Выводим на экран календарь с 3 месяцами в строке и 4 столбцами
         for (i = 1; i <= 4; ++i)
@@ -70,6 +87,10 @@ int main()
             horizontal = 1;
             vertical  += 9;
         }
+
+        counter = 1;            // Счетчик конца цикла
+        pasha_p(year, counter); // Сбрасываем дату Пасхи и Троицы
+
         gotoxy(1, 38);
         system ("pause");
         system ("cls");
@@ -125,7 +146,7 @@ void print_calendar (int month, int year, int horizontal, int vertical)
             tempDay = (temp1 + year + year / 4 - year / 100 + year / 400 + (31 * month + 10) / 12) % 7;
 
         // Если день праздничный в субботу или воскресенье, то подсвечиваем их
-        if (celebrate(year, month, i) == 1 && (tempDay == 5 || tempDay == 6))
+        if (celebrateDays[month - 1][i - 1] == 1 && (tempDay == 5 || tempDay == 6))
         {
             setcolor(BACKGROUNDRED);
             printf ("%3d", i);
@@ -139,7 +160,7 @@ void print_calendar (int month, int year, int horizontal, int vertical)
         }
 
         // Если день праздничный, то выделяем его красным
-        else if (celebrate(year, month, i) == 1)
+        else if (celebrateDays[month - 1][i - 1] == 1)
         {
             setcolor(RED);
             printf ("%3d", i);
@@ -155,46 +176,9 @@ void print_calendar (int month, int year, int horizontal, int vertical)
     setcolor(WHITE);
 }
 
-int celebrate (int year, int month, int i)
+void pasha_p (int year, int counter)
 {
-                                   //1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-    int celebrateDays[12][31] = {   {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Январь
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, // Февраль
-                                    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Март
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Апрель
-                                    {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Май
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, // Июнь
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Июль
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, // Август
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Сентябрь
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Октябрь
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Ноябрь
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}  // Декабрь
-                                };
-    int tmp1, y, temp_month, tmp2;
-    if (month == 5 && pasha_p(year) + 13 > 30)
-    {
-        celebrateDays[4][pasha_p(year) + 13 - 30] = 1;
-        tmp1 = pasha_p(year) + 13 - 30 + 50;
-        temp_month = month;
-        tmp2 = days_month[temp_month];
-        y = tmp1 - tmp2;
-        celebrateDays[++temp_month][y - 1] = 1;
-    }
-    else
-    {
-        celebrateDays[3][pasha_p(year) + 13] = 1;
-    }
-
-    if (celebrateDays[month - 1][i - 1] == 1)
-        return 1;
-    else
-        return 0;
-}
-
-int pasha_p (int year)
-{
-    int a, b, c, d, e;
+    int a, b, c, d, e, temp1, temp2, temp3, tempMonth;
 
     a = year % 19;
     b = year % 4;
@@ -202,5 +186,43 @@ int pasha_p (int year)
     d = (19 * a + 15) % 30;
     e = (2 * b + 4 * c + 6 * d + 6) % 7;
 
-    return (d + e - 9) - 1;
+    temp1 = (d + e - 9) - 1;
+    if (temp1 + 13 > 29)
+    {
+        celebrateDays[4][temp1 + 13 - 30] = 1;
+        temp3 = temp1 + 13 - 30 + 50;
+        temp2 = days_month[4];
+        temp3 = temp3 - temp2;
+        celebrateDays[5][temp3 - 1] = 1;
+    }
+    else
+    {
+        celebrateDays[3][temp1 + 13] = 1;
+        temp3 = temp1 + 13 + 50;
+        temp2 = days_month[3];
+        temp3 = temp3 - temp2;
+        if (temp3 - temp2 > 31)
+        {
+            tempMonth = 5;
+            celebrateDays[tempMonth][temp3 - 1] = 1;
+        }
+        else
+        {
+            tempMonth = 4;
+            celebrateDays[tempMonth][temp3 - 1] = 1;
+        }
+    }
+
+    // Сбрасываем даты Пасхи и Троицы
+    if (counter == 1 && temp1 + 13 > 29)
+    {
+        celebrateDays[4][temp1 + 13 - 30] = 0;
+        celebrateDays[5][temp3 - 1] = 0;
+    }
+
+    else if (counter == 1 && temp1 + 13 <= 29)
+    {
+        celebrateDays[3][temp1 + 13] = 0;
+        celebrateDays[tempMonth][temp3 - 1] = 0;
+    }
 }
