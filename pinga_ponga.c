08a4cf20f4ff266eb5019ce321f0c204d43436aa
d-serj <sp11kenny@gmail.com>
2017-01-26 16:57:05 +0200

    Update pinga_ponga.c

--- pinga_ponga.c
+++ pinga_ponga.c
@@ -1,6 +1,9 @@
 #include <curses.h>
 #include <windows.h>
 
+#define KEY_W 119
+#define KEY_S 115
+
 void init(void);
 
 struct coordinates
@@ -9,53 +12,71 @@ struct coordinates
     int x;
 };
 
+struct coordinates ball, racket_1, racket_2;
+
 int main()
 {
     int max_y       = 0, max_x = 0;
     int next_x      = 0;
     int next_y      = 0;
-    int direction_x = 3;            // Скорость изменения направления по X
+    int direction_x = 2;            // Скорость изменения направления по X
     int direction_y = 1;            // Скорость изменения направления по Y
-    int score       = 0;            // Счет
+    int score1      = 0;            // Счет игрока 1
+    int score2      = 0;            // Счет игрока 2
     int c;                          // Переменная хранения ввода с клавиатуры
-
-    struct coordinates racket;
-    struct coordinates ball;
-    ball.y   = 0;
-    ball.x   = 0;
-    racket.y = 0;
-    racket.x = 0;
+    int i;
 
     init();
+    getmaxyx(stdscr, max_y, max_x); // Получаем максимальные величины X и Y
 
-    getmaxyx(stdscr, max_y, max_x);
+    ball.y          = 0;
+    ball.x          = max_x / 2;
+    racket_1.y      = 0;
+    racket_1.x      = 0;
+    racket_2.y      = 0;
+    racket_2.x      = max_x - 1;
 
     while(1)
     {
         clear();
+        mvprintw(0, 20, "%2d", score1);
+        mvprintw(0, 45, "%2d", score2);
+
+        // Рисуем ракетки
+        for (i = 0; i <= 4; ++i)
+        {
+            mvaddch(racket_1.y + i, racket_1.x, ACS_BLOCK);
+            mvaddch(racket_2.y + i, racket_2.x, ACS_BLOCK);
+        }
+
         mvprintw(ball.y, ball.x, "o");
-        mvprintw(0, 10, "%d", score);
 
         // Управляем ракеткой
-		switch(c = getch())
-		{	case KEY_UP:
-                racket.y -= 1;
-				break;
-			case KEY_DOWN:
-				racket.y += 1;
-				break;
-			default:
-				break;
-		}
+        switch(c = getch())
+        {
+        case KEY_UP:
+            if (racket_2.y > 0)
+                racket_2.y -= 2;
+            break;
+        case KEY_W:
+            if (racket_1.y > 0)
+                racket_1.y -= 2;
+            break;
+        case KEY_DOWN:
+            if (racket_2.y <= max_y - 6)
+                racket_2.y += 2;
+            break;
 
-		// Рисуем ракетку
-        mvaddch(racket.y, racket.x, ACS_BLOCK);
-        mvaddch(racket.y + 1, racket.x, ACS_BLOCK);
-        mvaddch(racket.y + 2, racket.x, ACS_BLOCK);
-        mvaddch(racket.y + 3, racket.x, ACS_BLOCK);
+        case KEY_S:
+            if (racket_1.y <= max_y - 6)
+                racket_1.y += 2;
+            break;
+        default:
+            break;
+        }
 
         refresh();
-        Sleep(100);
+        Sleep(80);
 
         // Определяем положение мячика
         next_x = ball.x + direction_x;
@@ -65,11 +86,10 @@ int main()
         if (next_x >= max_x || next_x < 0 )
         {
             direction_x *= -1;
-            score += 1;
             MessageBeep(MB_ICONEXCLAMATION);
         }
 
-        if (next_y >= max_y || next_y < 0)
+        if (next_y == max_y || next_y < 0)
         {
             direction_y *= -1;
             MessageBeep(MB_ICONEXCLAMATION);
@@ -82,6 +102,13 @@ int main()
             ball.y += direction_y;
         }
 
+        // Если мячик не попал на ракетку увеличивает счет игрока 1
+        if (ball.x == 0 && !(ball.y >= racket_1.y && ball.y <= racket_1.y + 4))
+            score1 += 1;
+
+        else if (ball.x == max_x - 2 && !(ball.y >= racket_2.y && ball.y <= racket_2.y + 4))
+            score2 += 1;
+
     }
     endwin();
 }
@@ -93,4 +120,14 @@ void init(void)
     curs_set(FALSE);
     keypad(stdscr, TRUE);
     nodelay(stdscr, TRUE);
+    mvprintw(1, 0, ".---.  _                     .---.                        ");
+    mvprintw(2, 0, ": .; ::_;                    : .; :                       ");
+    mvprintw(3, 0, ":  _.'.-.,-.,-. .--.  .--.   :  _.'.--. ,-.,-. .--.  .--. ");
+    mvprintw(4, 0, ": :   : :: ,. :' .; :' .; :  : :  ' .; :: ,. :' .; :' .; :");
+    mvprintw(5, 0, ":_;   :_;:_;:_;`._. ;`.__.'  :_;  `.__.':_;:_;`._. ;`.__.'");
+    mvprintw(6, 0, "                .-. :                          .-. :      ");
+    mvprintw(7, 0, "                `._.'                          `._.'      ");
+    refresh();
+    system("pause");
 }
+
